28536281a37b6332bbf8a29ab5b481fd20eba777
agostinhodossantos <agostinhodossantos27@gmail.com>
2021-06-28 05:51:19 -0700

    Initial commit

---
+++ app/src/main/java/my/app/mz/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package my.app.mz.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
